f79604ccd2a968df1efae879d05aa9b37e525640
Lavanya Singh <lsingh@college.harvard.edu>
2021-02-11 17:55:58 -0500

    skeleton for the server done. all that is left is adding the actual response logic so blocked on the serialization library

--- server.py
+++ server.py
@@ -1,38 +1,54 @@
 #!/usr/bin/env python3
 
 import socket
-from _thread import *
 import threading 
 import userstate
 
 HOST = 'localhost'
 PORT = 12345
+
+# dictionary mapping usernames:UserStates for all users in the system
+# users are added when accounts are created and removed upon account deletion
+# note that the server has no persistent storage. this was a design decision
+# to reduce developer time. if this server needed to be more robust or reliable,
+# we would add persistent state.
 users = {}
 
 def thread_func(conn):
 
     user = None
+
+    # set a timeout of 0.5 seconds to avoid blocking on the socket
+    conn.settimeout(0.5)
     with conn:
         while True:
             
-            # This call will block if the connection is open but no data is being
-            # sent. In order to avoid message delivery delays, the client will 
-            # constantly ping the server, effectively converting to a polling model.
-            # This brings significant overhead because of the network traffic 
-            # being exchanged, but suffices for our small server.
-            # Design note: we alternatively could have made non-blocking calls to 
-            # conn.recv 
+            # exception will be raised if no data is available before the timout
+            try:
+                data = conn.recv(4096)
+            except socket.timeout:
+                None
 
-            data = conn.recv(4096)
-
-            # eventually this will be replaced with processing logic
+            # logic to process a message and then respond appropriately
+            # call jordan's library to derserialize the message
             print(data)
 
             if not user:
               # should have message type here or create account 
                 print("TODO")
               # set user to username 
+            else:
+                # switch on message type and process appropriately
+                # create account
+                # here 
+                # away
+                # email send -> bulk of work
+                # ping 
+                # list clients 
+                # delete account 
+                # show undelivered messages
 
+            # deliver any messages in the queue 
             while not users[user].deliver_now.empty():
                 message = users[user].deliver_now.get()
 
@@ -40,22 +56,27 @@ def thread_func(conn):
                 # message drops. note that the message will now be at the end of 
                 # the queue, so our server does not guarantee in-order message
                 # delivery.
+
+                # i'm worried that this call might timeout too bc conn.timeout(0.5)
+                # not sure if sends will also timeout?????????
                 try conn.sendall(message):
                     continue
-                except Exception e:
+                except e:
                     users[user].deliver_now.put(message)
-
-
 
 
 if __name__ == '__main__':
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
         s.bind((HOST, PORT))
         s.listen()
         print("server is listening")
 
         while True:
             conn, addr = s.accept()
-            start_new_thread(thread_func, (conn,))
+
+            # spawn a new thread to handle each connection to allow multiple connections
+            # this allows the server to maintain state for each connection 
+            threading.Thread(target=thread_func, (conn,)).start()
 
