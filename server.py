4fd8d6e92df336a28eeec63d4fd52d33cba93f21
Lavanya Singh <lsingh@college.harvard.edu>
2021-02-13 12:59:59 -0500

    kfsjda

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import socket
 import threading 
 import userstate
+from messages import * 
 
 HOST = 'localhost'
 PORT = 12345
@@ -25,15 +26,37 @@ def thread_func(conn):
             
             # exception will be raised if no data is available before the timout
             try:
-                data = conn.recv(4096)
+                message = conn.recv(4096)
             except socket.timeout:
                 None
+           
+            message_len = int.from_bytes(raw_message[8:13])
 
+            # message length does not include header length
+            current_len = len(raw_message - 12)
+            
+            # loop until we recieve the whole message
+            while current_len < message_len:
+                data = conn.recv(4096)
+                current_len += len(data)
+                raw_message += data
+            
+            message = messages.deserialize_message(raw_message)
+            message_type = type(message)
             # logic to process a message and then respond appropriately
             # call jordan's library to derserialize the message
-            print(data)
-
-            if not user:
+            
+            if (message_type == PingMessage):
+                response = PongMessage()
+                conn.send(response.serialize())
+            else if not user and (message_type not in [CreateAccountMessage, AwayMessage, HereMessage]):
+                response = ErrorMessage()
+                conn.send(response.seralize())
+            else if message_type == HereMessage:
+                user = message.username
+            else if message_type == CreateAccountMessage:
+                user = message.username
+                users.update({user: })
               # should have message type here or create account 
                 print("TODO")
               # set user to username 
@@ -47,6 +70,7 @@ def thread_func(conn):
                 # list clients 
                 # delete account 
                 # show undelivered messages
+                print("hi")
 
             # deliver any messages in the queue 
             while not users[user].deliver_now.empty():
@@ -59,14 +83,17 @@ def thread_func(conn):
 
                 # i'm worried that this call might timeout too bc conn.timeout(0.5)
                 # not sure if sends will also timeout?????????
-                try conn.sendall(message):
-                    continue
+                try:
+                    conn.sendall(message)
                 except e:
                     users[user].deliver_now.put(message)
 
 
 if __name__ == '__main__':
+    print(type(3))
+    print(type(3) == int)
 
+def ugh():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         s.bind((HOST, PORT))
@@ -78,5 +105,5 @@ if __name__ == '__main__':
 
             # spawn a new thread to handle each connection to allow multiple connections
             # this allows the server to maintain state for each connection 
-            threading.Thread(target=thread_func, (conn,)).start()
+            threading.Thread(target=thread_func, args=(conn,)).start()
 
