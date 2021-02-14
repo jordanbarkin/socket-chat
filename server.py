291b28ea0449470c5f9886ec2cee78b4adb93297
Jordan Barkin <jordanbarkin@college.harvard.edu>
2021-02-14 00:56:03 -0500

    refactored server

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 import socket
-import threading 
+import threading
 from userstate import *
-from messages import * 
+from messages import *
 
 HOST = 'localhost'
 PORT = 12345
@@ -15,129 +15,158 @@ PORT = 12345
 # we would add persistent state.
 users = {}
 
-# helper functions 
+# helper functions
 
 # send all new messages to a user over the connection
 def send_new_messages(user, conn):
     messages = []
+
     while user and not users[user].deliver_now.empty():
         messages.append(users[user].deliver_now.get())
-            
-        if len(messages) != 0:
-            conn.sendall(DeliverMessage(messages).serialize())
 
+    if messages:
+        conn.sendall(DeliverMessage(messages).serialize())
 
-def thread_func(conn):
+def read_message(conn):
+    # exception will be raised if no data is available before the timeout
+    try:
+        raw_message = conn.recv(4096)
+    except socket.timeout:
+        return None
 
+    # message length does not include headers
+    message_len = extract_length(raw_message) + 12
+    current_len = len(raw_message)
+
+    # loop until we recieve the whole message
+    # ToDo add a timeout here too
+    while current_len < message_len:
+        data = conn.recv(4096)
+        current_len += len(data)
+        raw_message += data
+
+    return deserialize_message(raw_message)
+
+def send_error_message(conn, err_msg):
+    response = ErrorMessage(err_msg)
+    conn.send(response.serialize())
+
+def handle_request(user, conn, message):
+    message_type = type(message)
+
+    print("Received a " + str(message_type))
+
+    # process message, switching on type
+    if message_type == PingMessage:
+        conn.send(PongMessage().serialize())
+
+    # users need to be "here" to use most features
+    elif not user and (message_type not in [CreateAccountMessage, HereMessage]):
+        send_error_message("Please log in or create an account before making requests.")
+
+    # similar to a login method, except that our server does not authenticate users :O
+    elif message_type == HereMessage:
+        try:
+            # users[message.username]
+            user = message.username
+            users[user].login()
+        except:
+            send_error_message("Account does not exist.")
+
+    # the user is also automatically marked as "here" for the newly created account
+    elif message_type == CreateAccountMessage:
+        user = message.username
+        users.update({user: UserState(user)})
+        users[user].login()
+
+        # add to permanent acccount list
+        with (open("users.txt", "w+")) as f:
+            f.write(user)
+
+    # again, like a logout without authentication
+    elif message_type == AwayMessage:
+        if user in users:
+            users[user].logout()
+            user = None
+        else:
+            send_error_message("Account does not exist")
+
+    # will be delivered immediately or on demand depending on target's "here" status
+    # note that add_message handles that logic
+    elif message_type == SendChatMessage:
+        target = message.username.strip()
+        print("Received email for ", target)
+        users[target].add_message((user, message.body))
+
+    # send back a list of all users
+    elif message_type == RequestUserListMessage:
+        response = UserListResponseMessage(list(users.keys()))
+        conn.send(response.serialize())
+
+    # you can only delete yourself for security reasons
+    elif message_type == DeleteAccountMessage:
+        if not user:
+            send_error_message("Please log in to delete your account.")
+        else:
+            users.pop(user)
+            user = None
+
+    # note that we deliver undelivered messages recieved both while the user was
+    # "away" and "here"
+    elif message_type == ShowUndeliveredMessage:
+        messages = []
+
+        while not users[user].deliver_later.empty():
+            messages.append(users[user].deliver_later.get())
+
+        while not users[user].deliver_now.empty():
+            messages.append(users[user].deliver_now.get())
+
+        response = DeliverMessage(messages)
+        conn.send(response.serialize())
+
+    return user
+
+def connection_thread(conn):
     user = None
 
     # set a timeout of 0.5 seconds to avoid blocking on the socket
     conn.settimeout(0.5)
     with conn:
         while True:
-            
-            # deliver any messages in the queue 
+            # deliver any messages in the user's queue
             send_new_messages(user, conn)
 
-            # exception will be raised if no data is available before the timeout
-            try:
-                raw_message = conn.recv(4096)
-            except socket.timeout:
-                continue
-           
-            # message length does not include headers
-            message_len = extract_length(raw_message) + 12
-            current_len = len(raw_message)
-            
-            # loop until we recieve the whole message
-            while current_len < message_len:
-                data = conn.recv(4096)
-                current_len += len(data)
-                raw_message += data
-            
-            message = deserialize_message(raw_message)
-            message_type = type(message)
-            print(message_type)
-           
-            # process message, switching on type
-            if message_type == PingMessage:
-                response = PongMessage()
-                conn.send(response.serialize())
-            # users need to be "here" to use most features
-            elif not user and (message_type not in [CreateAccountMessage, HereMessage]):
-                response = ErrorMessage("must be logged in to do that")
-                conn.send(response.serialize())
-            # similar to a login method, except that our server does not authenticate users :O
-            elif message_type == HereMessage:
-                try:
-                    users[message.username]
-                    user = message.username
-                    users[user].here = True
-                except:
-                    conn.send(ErrorMessage("user doesnt exist").serialize())
-            # the user is also automatically marked as "here" for the newly created account
-            elif message_type == CreateAccountMessage:
-                user = message.username
-                users.update({user: UserState(user)})
-                users[user].here = True
-                # persistent storage 
-                with (open("users.txt", "w+")) as f:
-                    f.write(user)
-            # again, like a logout without authentication
-            elif message_type == AwayMessage:
-                if user in users:
-                    users[user].here = False
-                    user = None
-                else:
-                    conn.send(ErrorMessage("you do not exist").serialize())
-            # will be delivered immediately or on demand depending on target's "here" status
-            # note that add_message handles that logic
-            elif message_type == SendChatMessage:
-                target = message.username.strip()
-                print("target ", target)
-                users[target].add_message((user, message.body))
-            elif message_type == RequestUserListMessage:
-                response = UserListResponseMessage(list(users.keys()))
-                print(list(users.keys()))
-                conn.send(response.serialize())
-            # you can only delete yourself for security reasons
-            elif message_type == DeleteAccountMessage:
-                if not user: 
-                    conn.send(ErrorMessage("must be here to delete account").serialize())
-                else:
-                    users.pop(user)
-                    user = None
-            # note that we deliver undelivered messages recieved both while the user was 
-            # "away" and "here"
-            elif message_type == ShowUndeliveredMessage:
-                messages = []
-                while not users[user].deliver_later.empty():
-                    print("hi")
-                    messages.append(users[user].deliver_later.get())
-                while not users[user].deliver_now.empty():
-                    messages.append(users[user].deliver_now.get())
-                response = DeliverMessage(messages)
-                conn.send(response.serialize())
+            # process any new request from the client
+            request = read_message(conn)
+
+            if request:
+                user = handle_request(user, conn, request)
 
 
-if __name__ == '__main__':
-
+def load_users(filename):
     with open("users.txt", "r") as f:
         lines = f.readlines()
         for line in lines:
             username = line.strip()
             users.update({username: UserState(username)})
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+if __name__ == '__main__':
+    # resume with users if file exists
+    try:
+        load_users("users.txt")
+    except:
+        pass
 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-        print("server is listening")
+        print("Server listening on", HOST, ":", PORT)
 
         while True:
             conn, addr = s.accept()
 
-            # spawn a new thread to handle each connection to allow multiple connections
-            # this allows the server to maintain state for each connection 
-            threading.Thread(target=thread_func, args=(conn,)).start()
+            # spawn a new thread to handle each connection to allow multiple simultaneous connections
+            # this allows the server to maintain state for each connection
+            threading.Thread(target=connection_thread, args=(conn,)).start()
 
--- userstate.py
+++ userstate.py
@@ -4,33 +4,33 @@ import queue
 
 class UserState:
     '''This class encapsulates all server-side information about a particular user.
-       
-       @attribute self.here: bool 
-           True if the user is currently "here," as in has sent a "here" message, and 
-           has an open connection to the server. 
+
+       @attribute self.here: bool
+           True if the user is currently "here," as in has sent a "here" message, and
+           has an open connection to the server.
 
        @attribute self.username: string
            Unique identifier for this user
 
        @attribute deliver_now: Queue
-           queue of messages to be delivered immediately. These are messages that 
-           were received while the user was "here", and so should be delivered 
-           immediately according to the problem set specifications. 
+           queue of messages to be delivered immediately. These are messages that
+           were received while the user was "here", and so should be delivered
+           immediately according to the problem set specifications.
 
        @attribute deliver_later: Queue
            queue of messages to be delivered on-demand when the user is "here." These
-           are messages that were received when the user was "away." 
+           are messages that were received when the user was "away."
 
        @method add_message: message: (str, str) -> None
-           add a message to the appropriate queue depending on whether or not the 
-           user is "here" Will be used by other threads to send messages to 
-           this user. 
-           @param: (sender, body) a tuple consisting of the sender's username and the 
+           add a message to the appropriate queue depending on whether or not the
+           user is "here" Will be used by other threads to send messages to
+           this user.
+           @param: (sender, body) a tuple consisting of the sender's username and the
                    message body
 
        @notes
-           Note that UserState has no memory of past messages. Both message queue 
-           contain only undelivered messages. Also note that the server has no 
+           Note that UserState has no memory of past messages. Both message queue
+           contain only undelivered messages. Also note that the server has no
            persistent storage. This design decision was made to reduce developer time,
            but it comes at the cost of robustness. If we were extending this server,
            a historical record of messages and persistent storage would be high-
@@ -42,7 +42,7 @@ class UserState:
            the caller's responsibility.
 
            deliver_now and deliver_later are Queues and not lists because they need to
-           support concurrent access by multiple threads. This user's thread may pop 
+           support concurrent access by multiple threads. This user's thread may pop
            messages off the queues to deliver them, and other user threads may add
            messages to the queues.
 
@@ -55,14 +55,22 @@ class UserState:
 
     def add_message(self, message: (str,str)):
         '''Other user threads call this method to add a message to this user's queue.
-           Note a possible race condition: if self.here changes between method invocation 
-           and method exceution then the message may be pushed onto the wrong queue. We 
+           Note a possible race condition: if self.here changes between method invocation
+           and method exceution then the message may be pushed onto the wrong queue. We
            deem this race condition minor because the message will be still be delivered,
            it just may be delivered later than the sender will expect.
         '''
-        print("here: ", self.here)
         if self.here:
-            print("here")
             self.deliver_now.put(message)
         else:
             self.deliver_later.put(message)
+
+    def login(self):
+        self.here = True
+
+    def logout(self):
+        self.here = False
+
+
+
+
